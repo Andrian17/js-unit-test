25eb0f26808de01202891e077950231fc87b41d0
Andrian17 <andriancimen123@gmail.com>
2024-01-10 22:47:29 +0800

    it function

--- test/sum.test.js
+++ test/sum.test.js
@@ -4,3 +4,9 @@ test("test sum function sum(1, 2) must be 3", () => {
     const result = sum(1, 2);
     expect(result).toBe(3);
 })
+
+describe("Test it function", () => {
+    it("SUM test", () => {
+        expect(sum(20, 30)).toBe(50);
+    });
+});
