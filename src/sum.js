ce7081fd1ec04f59fd572d8043424b8a3147673e
Andrian17 <andriancimen123@gmail.com>
2024-01-10 23:24:24 +0800

    each function

--- src/sum.js
+++ src/sum.js
@@ -2,3 +2,11 @@
 export const sum = (first, second) => {
     return first + second
 }
+
+export const sumAll = (numbers) => {
+    let result = null;
+    numbers.forEach(element => {
+        result += element
+    });
+    return result;
+}
--- test/sum.test.js
+++ test/sum.test.js
@@ -1,4 +1,4 @@
-import { sum } from "../src/sum";
+import { sum, sumAll } from "../src/sum";
 
 test("test sum function sum(1, 2) must be 3", () => {
     const result = sum(1, 2);
@@ -16,3 +16,42 @@ describe("Test it function", () => {
         expect(sum(20, 30)).toBe(50);
     });
 });
+
+const tableNumber1 = [
+    [
+        [10, 10, 20], 
+        40
+    ],
+    [
+        [1, 2, 3], 
+        6
+    ],
+    [
+        [3, 3, 4], 
+        10
+    ]
+];
+
+test.each(tableNumber1)("test 2 sumAll(%s) should %i", (numbers, expected) => {
+    expect(sumAll(numbers)).toBe(expected);
+});
+
+
+const tableNumber2 = [
+    {
+        numbers: [10, 10, 20], 
+        expected: 40
+    },
+    {
+        numbers: [1, 2, 3], 
+        expected: 6
+    },
+    {
+        numbers: [3, 3, 4], 
+        expected: 10
+    }
+];
+
+test.each(tableNumber2)("test sumAll($numbers) should $expected", ({numbers, expected}) => {
+    expect(sumAll(numbers)).toBe(expected);
+})
